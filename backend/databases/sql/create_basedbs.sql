274bbb0dfbdeed39c4752b01bcc76f58d98505f0
WeirdnessUnfolds <simon.langstrom@hotmail.se>
2023-11-20 16:49:50 +0100

    Added rudimentary database and modified docker-compose, added mysql.

--- backend/databases/sql/create_basedbs.sql
+++ backend/databases/sql/create_basedbs.sql
@@ -1,24 +1,9 @@
 CREATE USER 'adm'@'%' IDENTIFIED BY 'pass';
 GRANT ALL PRIVILEGES ON adm.* TO 'adm'@'%';
 
-CREATE DATABASE user;
-USE doe;
+CREATE DATABASE users_andbikes;
+USE users_andbikes;
 
-CREATE TABLE doe ( namn CHAR(10) );
-INSERT INTO doe VALUES ("Jane Doe"), ("John Doe");
+CREATE TABLE user ( userId INT, username VARCHAR(45), email VARCHAR(45), password(VARCHAR(45)));
 
-
-CREATE DATABASE scooter;
-USE scooter;
-
-
-
-CREATE DATABASE point
-USE point;
-
-
-CREATE database city;
-use city;
-
-create DATABASE zones;
-use zones;
+CREATE TABLE scooter (scooterId INT, lon FLOAT, )
