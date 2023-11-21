335c9343afba47e3ac8ca64a1490b57e980dbdc1
WeirdnessUnfolds <simon.langstrom@hotmail.se>
2023-11-21 14:03:56 +0100

    Added proper constraints and prettified the code.

--- backend/databases/sql/create_basedbs.sql
+++ backend/databases/sql/create_basedbs.sql
@@ -1,9 +1,40 @@
-CREATE USER 'adm'@'%' IDENTIFIED BY 'pass';
-GRANT ALL PRIVILEGES ON adm.* TO 'adm'@'%';
-
-CREATE DATABASE users_andbikes;
-USE users_andbikes;
-
-CREATE TABLE user ( userId INT, username VARCHAR(45), email VARCHAR(45), password(VARCHAR(45)));
-
-CREATE TABLE scooter (scooterId INT, lon FLOAT, )
+CREATE USER 'adm'@'%' IDENTIFIED BY 'pass'; GRANT ALL PRIVILEGES
+ON
+    adm.* TO 'adm'@'%';
+CREATE DATABASE users_andbikes; USE
+    users_andbikes;
+CREATE TABLE Users(
+    userId INT NOT NULL,
+    username VARCHAR(45) NOT NULL,
+    email VARCHAR(45) NOT NULL,
+    passwd VARCHAR(45) NOT NULL,
+    PRIMARY KEY(userId)
+); CREATE TABLE City(
+    cityId INT NOT NULL,
+    id VARCHAR(45) NOT NULL,
+    lat FLOAT NOT NULL,
+    lon FLOAT NOT NULL
+); CREATE TABLE Scooter(
+    scooterId INT NOT NULL,
+    lon FLOAT,
+    city_cityid INT,
+    PRIMARY KEY(scooterId),
+    FOREIGN KEY(city_cityid) REFERENCES city(cityId)
+); CREATE TABLE Zones(
+    pointname VARCHAR(45),
+    zoneId INT NOT NULL,
+    city_cityid INT NOT NULL,
+    zonetype VARCHAR(20) NOT NULL,
+    PRIMARY KEY(zoneId),
+    FOREIGN KEY(city_cityid) REFERENCES city(cityId)
+); CREATE TABLE UsertoBike(
+    idUsertobike INT NOT NULL,
+    user_userid INT,
+    scooterId INT,
+    startTime TIMESTAMP NOT NULL,
+    stopTime TIMESTAMP NOT NULL,
+    price FLOAT,
+    PRIMARY KEY(idUsertobike),
+    FOREIGN KEY(user_userid) REFERENCES users(userId),
+    FOREIGN KEY(scooterId) REFERENCES scooter(scooterId)
+);
