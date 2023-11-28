f27454c31aa17398f05bfbc667c61024c66e914e
Albin Stegler <albin.stegler@gmail.com>
2023-11-28 14:57:27 +0100

    added simulation as separate react project

---
+++ simulation/sim/src/components/Bikes.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function bikes() {
+    return (
+        <div className="flex flex-col items-center justify-center"></div>
+
+    );
+}
+
+export default bikes;
---
+++ simulation/sim/src/util/withAuth.js
@@ -0,0 +1,20 @@
+import React, { useContext, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { AuthContext } from './AuthContext';
+
+const withAuth = (WrappedComponent) => {
+    return (props) => {
+        const { isLoggedIn } = useContext(AuthContext);
+        const navigate = useNavigate();
+
+        useEffect(() => {
+            if (!isLoggedIn) {
+                navigate('/');
+            }
+        }, [isLoggedIn, navigate]);
+
+        return isLoggedIn ? <WrappedComponent {...props} /> : null;
+    };
+};
+
+export default withAuth;
