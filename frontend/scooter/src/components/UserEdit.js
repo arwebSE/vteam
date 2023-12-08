b8c20ae1ec1e66b78835895bf3450071ab2391fd
August R <aggesoft@gmail.com>
2023-12-08 19:45:36 +0100

    user edit logic
    * changed useredit logic
    * changed userlist logic
    * updated userlist styling

--- frontend/scooter/src/components/UserEdit.js
+++ frontend/scooter/src/components/UserEdit.js
@@ -1,82 +1,92 @@
-import React, { useState, useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { useLocation, useNavigate, Link } from "react-router-dom";
 
 import userModel from "../models/userModel";
 
 const UserEdit = () => {
-
     const location = useLocation();
-    const lastPartOfUrl = location.pathname.split("/").pop();
+    const navigate = useNavigate();
+    const userId = location.pathname.split("/").pop();
+
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [role, setRole] = useState("user");
 
     useEffect(() => {
         const fetchUser = async () => {
-            let users = await userModel.getUser(lastPartOfUrl);
-            setName(users.username);
-            setEmail(users.email);
-            setPasswd(users.passwd);
-
-            if (users.userrole === null || users.userrole === "user") {
-                document.getElementById("select").selectedIndex = 1;
-            }
-            else {
-                document.getElementById("select").selectedIndex = 0;
+            try {
+                const user = await userModel.getUser(userId);
+                setUsername(user.username);
+                setEmail(user.email);
+                setPassword(user.passwd);
+                setRole(user.userrole || "user");
+            } catch (error) {
+                console.error("Failed to fetch user:", error);
+                // todo: handle fetch error?
             }
         };
 
         fetchUser();
-    }, [lastPartOfUrl]);
-    const [username, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [passwd, setPasswd] = useState('');
-
-    const handleNameChange = (event) => {
-        setName(event.target.value);
-    };
-
-    const handleEmailChange = (event) => {
-        setEmail(event.target.value);
-    };
-
-    const handlePasswdChange = (event) => {
-        setPasswd(event.target.value);
-    }
+    }, [userId]);
 
     const handleSubmit = async (event) => {
         event.preventDefault();
-        let selectedOption = document.getElementById("select").value;
-        await userModel.editUser(lastPartOfUrl, username, email, passwd, selectedOption);
-
-        console.log("User updated successfully.")
-        // Fetch the updated user data
-        const updatedUser = await userModel.getUser(lastPartOfUrl);
-        // Update the state variables with the new data
-        setName(updatedUser.username);
-        setEmail(updatedUser.email);
-        setPasswd(updatedUser.passwd);
+        try {
+            await userModel.editUser(userId, username, email, password, role);
+            console.log("User updated successfully.");
+            navigate("/admin/user");
+        } catch (error) {
+            console.error("Failed to update user:", error);
+            // todo: handle submit error?
+        }
     };
 
     return (
-        <form className='w-11/12 m-1 p-5 bg-slate-300 rounded flex flex-col items-center justify-center gap-5' onSubmit={handleSubmit}>
-            <label className='text-lg font-bold'>
-                Username:
-            </label>
-            <input className='rounded ml-2 w-4/5 p-2 font-extralight' type="text" value={username} onChange={handleNameChange} />
-            <label className='text-lg font-bold'>
-                Email:
-            </label>
-            <input className='rounded w-4/5 ml-2 p-2 font-extralight' type="email" value={email} onChange={handleEmailChange} />
-            <label className='text-lg font-bold'>
-                Password:
-            </label>
-            <input className='rounded ml-2 w-4/5 p-2 font-extralight' type="password" value={passwd} onChange={handlePasswdChange} />
-            <label className='text-lg font-bold'>
-                User Role:
-            </label>
-            <select id="select" className='rounded ml-2 p-2 font-extralight'>
+        <form
+            onSubmit={handleSubmit}
+            className="w-11/12 m-1 p-5 bg-slate-300 rounded flex flex-col items-center justify-center gap-5"
+        >
+            <label className="text-lg font-bold">Username:</label>
+            <input
+                className="rounded ml-2 w-4/5 p-2 font-extralight"
+                type="text"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+            />
+
+            <label className="text-lg font-bold">Email:</label>
+            <input
+                className="rounded w-4/5 ml-2 p-2 font-extralight"
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+            />
+
+            <label className="text-lg font-bold">Password:</label>
+            <input
+                className="rounded ml-2 w-4/5 p-2 font-extralight"
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+            />
+
+            <label className="text-lg font-bold">User Role:</label>
+            <select
+                value={role}
+                onChange={(e) => setRole(e.target.value)}
+                className="rounded ml-2 p-2 font-extralight"
+            >
                 <option value="admin">Admin</option>
                 <option value="user">User</option>
             </select>
-            <button className='w-20 rounded text-lg bg-black p-2 text-white' type="submit">Save</button>
+
+            <button
+                className="w-20 rounded text-lg bg-black p-2 text-white"
+                type="submit"
+            >
+                Save
+            </button>
         </form>
     );
 };
--- frontend/scooter/src/components/UserList.js
+++ frontend/scooter/src/components/UserList.js
@@ -1,49 +1,50 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import userModel from "../models/userModel.js";
 import { useNavigate } from "react-router-dom";
 
 const UserList = () => {
-    const navigate = useNavigate();
     const [users, setUsers] = useState([]);
+    const navigate = useNavigate();
 
     useEffect(() => {
-        async function fetchUsers() {
-            const usersData = await userModel.getUsers();
-            setUsers(usersData);
-        }
+        const fetchUsers = async () => {
+            const fetchedUsers = await userModel.getUsers();
+            setUsers(fetchedUsers);
+        };
+
         fetchUsers();
     }, []);
 
-    function clickHandler(userId) {
-        navigate(`/admin/user/${userId}`);
-    }
+    const clickHandler = (userid) => {
+        navigate(`/admin/user/${userid}`);
+    };
 
     return (
-        <div className="flex flex-col">
-            <table className="min-w-full">
-                <thead>
-                    <tr className="bg-indigo-600 text-white">
-                        <th className="py-2 px-4 text-left">Username</th>
-                        <th className="py-2 px-4 text-left">Email</th>
-                        <th className="py-2 px-4 text-right">UserID</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {users.map((user) => (
-                        <tr
-                            key={user.userId}
-                            onClick={() => clickHandler(user.userId)}
-                            className="cursor-pointer hover:bg-indigo-100"
-                        >
-                            <td className="py-2 px-4">{user.username}</td>
-                            <td className="py-2 px-4">{user.email}</td>
-                            <td className="py-2 px-4 text-right">
-                                {user.userId}
-                            </td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
+        <div className="flex flex-col p-6 bg-gray-100 rounded-lg shadow-md">
+            <div className="flex flex-row justify-between text-lg mb-4">
+                <h1 className="font-bold text-gray-700">Users</h1>
+                <h1 className="font-bold text-gray-700">ID</h1>
+            </div>
+
+            <div className="flex flex-col gap-2">
+                {users.map((user) => (
+                    <div
+                        key={user.id}
+                        onClick={() => clickHandler(user.userId)}
+                        className="cursor-pointer rounded-lg hover:bg-slate-200 flex flex-row bg-white justify-between items-center p-4 shadow-sm transition hover:shadow-md transition duration-300 ease-in-out"
+                    >
+                        <div className="flex flex-col">
+                            <h2 className="text-lg font-semibold text-gray-800">
+                                {user.username}
+                            </h2>
+                            <p className="text-gray-600">{user.email}</p>
+                        </div>
+                        <h1 className="text-lg font-semibold text-gray-800">
+                            {user.userId}
+                        </h1>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 };
