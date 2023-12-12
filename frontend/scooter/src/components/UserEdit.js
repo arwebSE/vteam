e23db563ed0454e0d5b8c548625034c52d2c44d1
August M R <august.rosenqvist@scania.com>
2023-12-12 16:11:40 +0100

    admin user
    * combined views to 1 view parent component
    * slight changes to useredit and userlist

--- frontend/scooter/src/components/UserEdit.js
+++ frontend/scooter/src/components/UserEdit.js
@@ -1,33 +1,47 @@
 import React, { useState, useEffect } from "react";
-import { useLocation, useNavigate, Link } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
+import { MdArrowBack } from "react-icons/md";
+import { SpinnerDotted } from "spinners-react";
 
 import userModel from "../models/userModel";
 
 const UserEdit = () => {
     const location = useLocation();
     const navigate = useNavigate();
-    const userId = location.pathname.split("/").pop();
-
+    const [loading, setLoading] = useState(false);
     const [username, setUsername] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [role, setRole] = useState("user");
 
-    useEffect(() => {
-        const fetchUser = async () => {
-            try {
-                const user = await userModel.getUser(userId);
-                setUsername(user.username);
-                setEmail(user.email);
-                setPassword(user.passwd);
-                setRole(user.userrole || "user");
-            } catch (error) {
-                console.error("Failed to fetch user:", error);
-                // todo: handle fetch error?
-            }
-        };
+    const userId = location.pathname.split("/").pop();
 
+    const fetchUser = async () => {
+        setLoading(true);
+        try {
+            const user = await userModel.getUser(userId);
+            setUsername(user.username);
+            setEmail(user.email);
+            setPassword(user.passwd);
+            setRole(user.userrole || "user");
+        } catch (error) {
+            console.error("Failed to fetch user:", error);
+            // todo: handle fetch error?
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    useEffect(() => {
         fetchUser();
+
+        return () => {
+            // component unmount cleanup
+            setUsername("");
+            setEmail("");
+            setPassword("");
+            setRole("user");
+        };
     }, [userId]);
 
     const handleSubmit = async (event) => {
@@ -42,14 +56,44 @@ const UserEdit = () => {
         }
     };
 
+    const handleBack = () => {
+        navigate("/admin/user");
+    };
+
+    if (loading) {
+        return (
+            <div className="h-screen flex items-center justify-center">
+                <SpinnerDotted
+                    size={50}
+                    thickness={100}
+                    speed={100}
+                    color="rgb(79 70 229)"
+                />
+            </div>
+        );
+    }
+
     return (
         <form
             onSubmit={handleSubmit}
             className="flex flex-col bg-gray-100 rounded-lg shadow-xl p-12"
         >
-            <h1 className="text-2xl font-bold text-gray-800 mb-6 text-center">
+            <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold text-indigo-600 mb-6 text-center">
                 Edit User
             </h1>
+
+            <p className="rounded p-3 text-center text-sm md:text-base lg:text-lg text-gray-700 mb-4 shadow bg-white">
+                Fill in the info that you want to save and press save when done
+            </p>
+
+            <div className="flex justify-start mb-6">
+                <button
+                    onClick={handleBack}
+                    className="cursor-pointer rounded bg-indigo-600 hover:bg-indigo-700 text-lg p-3 text-white shadow-md hover:shadow-lg transition duration-300 ease-in-out flex items-center"
+                >
+                    <MdArrowBack className="mr-2" /> Back to User List
+                </button>
+            </div>
 
             <label className="text-lg font-semibold mb-2">Username:</label>
             <input
--- frontend/scooter/src/components/UserList.js
+++ frontend/scooter/src/components/UserList.js
@@ -4,16 +4,18 @@ import { useNavigate } from "react-router-dom";
 
 const UserList = () => {
     const [users, setUsers] = useState([]);
+    const [refresh, setRefresh] = useState(false);
     const navigate = useNavigate();
 
-    useEffect(() => {
-        const fetchUsers = async () => {
-            const fetchedUsers = await userModel.getUsers();
-            setUsers(fetchedUsers);
-        };
+    const fetchUsers = async () => {
+        const fetchedUsers = await userModel.getUsers();
+        setUsers(fetchedUsers);
+        setRefresh(false);
+    };
 
+    useEffect(() => {
         fetchUsers();
-    }, []);
+    }, [refresh]);
 
     const clickHandler = (userid) => {
         navigate(`/admin/user/${userid}`);
@@ -21,6 +23,10 @@ const UserList = () => {
 
     return (
         <div className="flex flex-col p-6 bg-gray-100 rounded-lg shadow-md">
+            <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold text-indigo-600 mb-6 text-center">
+                Edit Users
+            </h1>
+
             <div className="flex flex-row justify-between text-lg mb-4">
                 <h1 className="font-bold text-gray-700">Users</h1>
                 <h1 className="font-bold text-gray-700">ID</h1>
