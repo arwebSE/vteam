94a472cb09a6e86be6986fe8deb5112bd6c479fe
Albin Stegler <albin.stegler@gmail.com>
2023-12-13 13:07:47 +0100

    added a backbutton

--- frontend/scooter/src/views/Admin/Bike/AdminBike.js
+++ frontend/scooter/src/views/Admin/Bike/AdminBike.js
@@ -1,12 +1,20 @@
 import withAuth from "../../../util/withAuth";
-
+import { Link } from "react-router-dom";
 import "./style.css";
 import logo from "../../../logo.png";
 import MarkMap from "../../../components/markLocationMap";
 function AdminBike() {
 
     return (
-        <MarkMap />
+        <div className="flex flex-col w-full mt-12 pt-10">
+            <Link
+                to="/admin"
+                className="m-2 w-fit cursor-pointer rounded bg-indigo-600 hover:bg-indigo-700 text-lg p-3 text-white shadow-md hover:shadow-lg transition duration-300 ease-in-out"
+            >
+                Back
+            </Link>
+            <MarkMap />
+        </div>
     );
 }
 
