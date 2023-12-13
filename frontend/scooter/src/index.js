4e9056f5ffe76356bc8e8a6a8505ccb229278ca6
Albin Stegler <albin.stegler@gmail.com>
2023-12-13 12:44:21 +0100

    add/edit bikes v1 done

--- frontend/scooter/src/index.js
+++ frontend/scooter/src/index.js
@@ -14,7 +14,6 @@ import AdminZone from "./views/Admin/Zone/AdminZone";
 import EditUser from "./views/Admin/EditUser/EditUser";
 import EditBike from "./views/Admin/EditBike/EditBike";
 
-
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
         <AuthProvider>
