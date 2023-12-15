064f095a44a1bbb3feb57e5f8a94cf27fbbec100
WeirdnessUnfolds <simon.langstrom@hotmail.se>
2023-12-15 16:12:27 +0100

    Added Passverif to backend for login

--- backend/models/user.js
+++ backend/models/user.js
@@ -28,11 +28,12 @@ const userModel = {
     /**
      * A function that verifies the password.
      * @param {string} passwd - The password to be verified.
+     * @param {number} id - The ID of the user.
      * @param {object} res - The response object.
      * @returns {object} The JSON representation of the results.
      */
-    passVerif: function (passwd, res) {
-        db.get('SELECT * FROM Users WHERE passwd = ?', passwd, function (error, results, fields) {
+    passVerif: function (id, passwd, res) {
+        db.get('SELECT * FROM Users WHERE passwd = ? AND userID = ?	', [passwd, id], function (error, results, fields) {
             if (error) throw error;
              return res.json(results);
         });
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -3,9 +3,10 @@ const router = express.Router();
 const userModel = require('../models/user');
 router.get("/", (req, res) => userModel.getAll(res));
 router.get("/:userId", (req, res) => userModel.getOne(req.params.userId, res));
+router.get("(userId/passwd)", (req, res) => userModel.passVerif(req.params.userId, req.params.passwd, res));
 router.post("/", (req, res) => userModel.create(req, res));
 router.put("/:userId", (req, res) => userModel.update(req.params.userId, req.body, res));
 router.delete("/:userId", (req, res) => userModel.delete(req.params.userId, res));
-
+router.get("/")
 
 module.exports = router;
