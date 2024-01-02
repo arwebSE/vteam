6949266f28b5dfd4c8bcacc3ca895e0e7273b293
WeirdnessUnfolds <simon.langstrom@hotmail.se>
2024-01-02 17:16:15 +0100

    Added user endpoint tests

--- backend/src/test/backend.test.js
+++ backend/src/test/backend.test.js
@@ -1,24 +1,61 @@
-import axios from 'axios';
-import MockAdapter from 'axios-mock-adapter';
+import axios from "axios"
 
+describe("User tests", () => {
 
-// Test: Adding a user to the database
+test("Adding a user", async () => {
 
-const mockadapter = new MockAdapter(axios);
-
-test("Test: Adding a user to the database", () => {
-
+    var username = "username"
+    var email = "email@example.com"
+    var passwd = "passwd"
+    
     const jsonData = {
-        username: 'testUser',
-        email: 'test@example.com',
-        passwd: 'testpass'
+        username: username,
+        email: email,
+        passwd: passwd
     };
 
-
-    mockadapter.onPost('http://localhost:1337/user').reply(200, {});
-
-    return axios.post('http://localhost:1337/user', jsonData).then((res) => {
-        expect(res.status).toBe(200);
-    })
     
+    // Send a POST request to your API endpoint
+    const response = await fetch('http://localhost:1337/user', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json' // Set content type to JSON
+        },
+        body: JSON.stringify(jsonData)
+    });
+    expect(response.status).toBe(201);
+
+
+    
+})
+
+
+test("Getting all users", async () => {
+    const response = await axios.get('http://localhost:1337/user');
+    expect(response.status).toBe(200);
+    console.log(response.data);
+})
+
+test("Getting one user", async () => {
+    const response = await axios.get('http://localhost:1337/user/1');
+    expect(response.status).toBe(200);
+    console.log(response.data);
+})
+
+test("Deleting a user", async () => {
+ 
+
+       // Send a POST request to your API endpoint
+       const response = await fetch('http://localhost:1337/user/1', {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json' // Set content type to JSON
+        },
+    });
+    expect(response.status).toBe(200);
+
+
+});
+
+
 })
