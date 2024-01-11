935e9237ecaf418ca58268cb116afac6ad55fc6b
AlbinStegler <117485527+AlbinStegler@users.noreply.github.com>
2024-01-11 16:23:26 +0100

    Update start.bash

--- start.bash
+++ start.bash
@@ -1,4 +1,4 @@
-echo -e "ENV='dev'\nAPI-KEY='BOI-API-KEY'" > ./backend/.env
+echo -e "ENV='dev'\nAPIKEY='BOI-API-KEY'" > ./backend/.env
 npm install --prefix ./frontend/scooter
 npm install --prefix ./backend
 docker-compose up -d frontend
