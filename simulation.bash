f758687d702f790265058ee8987026509011c93f
Albin Stegler <albin.stegler@gmail.com>
2024-01-11 16:15:17 +0100

    bash modified

--- simulation.bash
+++ simulation.bash
@@ -1,4 +1,4 @@
-echo -e "ENV='simulation'\nAPI-KEY='BOI-API-KEY'" > ./backend/.env
+echo -e "ENV='simulation'\nAPIKEY='BOI-API-KEY'" > ./backend/.env
 npm install --prefix ./frontend/scooter
 npm install --prefix ./backend
 docker-compose up -d frontend
